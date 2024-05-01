1013719cc8ecea24451ae3ddaffecece39738a94
jimmy southavy <you@example.com>
2024-05-01 21:43:23 +0700

    Myproject

---
+++ src/main/java/com/example/demo/WebformycompanynewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebformycompanynewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebformycompanynewApplication.class, args);
+	}
+
+}
